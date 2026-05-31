d160bd13de76cf47fd7171b737c40082e289320a
Modala Swathi <swathimodala26@gmail.com>
2026-05-31 14:37:09 +0530

    Initial Commit

---
+++ UserContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({
+    firstName: '',
+    lastName: '',
+    phone: '',
+    profilePicture: null,
+  });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
